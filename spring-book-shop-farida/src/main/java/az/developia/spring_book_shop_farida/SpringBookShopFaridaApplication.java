1ccc91b0b0670b3a0e579005d6f210a3a9a628bd
farida2498 <125817327+faridaheydarova@users.noreply.github.com>
2024-11-09 16:31:06 +0400

    spring project quruldu

---
+++ spring-book-shop-farida/src/main/java/az/developia/spring_book_shop_farida/SpringBookShopFaridaApplication.java
@@ -0,0 +1,13 @@
+package az.developia.spring_book_shop_farida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookShopFaridaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookShopFaridaApplication.class, args);
+	}
+
+}
---
+++ spring-book-shop-farida/src/test/java/az/developia/spring_book_shop_farida/SpringBookShopFaridaApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.spring_book_shop_farida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookShopFaridaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
